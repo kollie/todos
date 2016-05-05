5c7dc136cfc45c0adc3cf4b4bfee674afa29d4a5
Israel Kollie <zikollie@gmail.com>
2016-05-05 14:24:24 +0000

    Add todo-user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,44 +19,39 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
+var User = sequelize.define('User', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
 	//force: true
 }).then(function() {
 	console.log('Everything is synced');
 
 
-	Todo.findById(2).then(function (todo) {
-		if (todo) {
-			console.log(todo.toJSON());
-		} else{
-			console.log('todo not found');
-		}
-	});
-
-	// Todo.create({
-	// 	description: 'Going to the bank'
-	// }).then(function (todo) {
-	// 	return Todo.create({
-	// 		description: 'Eating cassava'
-	// 	});
+	// User.create({
+	// 	email: 'zikollie@gmail.com'
 	// }).then(function () {
-	// 	//return Todo.findById(1)
-	// 	return Todo.findAll({
-	// 		where: {
-	// 			description: {
-	// 				$like: '%to%'
-	// 			}
-	// 		}
+	// 	return Todo.create({
+	// 		description: 'Eating beans'
 	// 	});
-	// }).then(function(todos) {
-	// 	if (todos) {
-	// 		todos.forEach(function (todo) {
-	// 			console.log(todo.toJSON());
-	// 		})
-	// 	} else { 
-	// 		console.log('no todo found');
-	// 	}
-	// }).catch(function (e) {
-	// 	console.log(e);
+	// }).then(function (todo) {
+	// 	User.findById(1).then(function (user) {
+	// 		user.addTodo(todo);
+	// 	});
 	// });
+	User.findById(1).then(function (user) {
+		user.getTodos({
+			where:{
+				completed: false
+			}
+		}).then(function (todos) {
+			todos.forEach(function (todo) {
+				console.log(todo.toJSON());
+			});
+		});
+	});
 });
